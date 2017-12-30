fc26fed16fb17819d6cb09f84386870e3cee7697
Administrator <Administrator@UMZYHEMP3VOMU7X>
2017-12-30 10:16:11 +0800

    comit

--- eurekaClient/src/main/java/com/rb/sz/cmb/Application.java
+++ eurekaClient/src/main/java/com/rb/sz/cmb/Application.java
@@ -1,10 +1,29 @@
 package com.rb.sz.cmb;
 
+import org.codehaus.jettison.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@EnableEurekaClient
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
 		 SpringApplication.run(Application.class, args);
 	}
+	
+	@Value("${server.port}")
+	private String port;
+	@RequestMapping(value="/hi",method=RequestMethod.POST)
+	public String hi(@RequestParam JSONObject name){
+		return "i am "+name+" from port="+port;
+	}
+	
+	
 }
